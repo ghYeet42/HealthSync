854d4cc26145598e4e5f5eb4a98a17a0b45d8796
ghyeet42 <carterfscarlett@gmail.com>
2024-02-02 10:46:50 -0500

    almost

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
+con = pymysql.connect (
+    database = "cscarlett_healthsync",
+    user = "cscarlett",
+    password = "228941274",
+    host = "10.100.33.60",
+    cursorclass = pymysql.cursors.DictCursor
+)
+
 ######
 
 @app.route("/")
@@ -18,3 +26,29 @@ def index():
     return render_template ("home.html.jinja")
 
     # return ("<p style=\"color:red;\">Hello!</p>")
+
+@app.route("/register", methods=["POST", "GET"])
+def signup():
+
+    if request.method == "POST":
+
+        newUserProfilePicture = request.form["pfp"]
+
+        newUserEmail = request.form["email"]
+
+        newUserUsername = request.form["username"]
+
+        newUserPassword = request.form["password"]
+
+        newUserBirthday = request.form["birthday"]
+        
+
+        cursor = con.cursor()
+
+        cursor.execute(f"INSERT INTO `users` (`pfp`, `email`, `username`, `password`, `birthday`) VALUES ('{newUserProfilePicture}', '{newUserEmail}', '{newUserUsername}', '{newUserPassword}', '{newUserBirthday}'")
+
+        cursor.close()
+
+        con.commit()
+
+    return render_template ("signup.html.jinja")
